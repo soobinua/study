f4d983062be8f2cdadff2129283de09943392c8b
soobinua <dev7er@gmail.com>
2023-06-29 14:03:18 +0900

    구구단 - 반복문

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,18 +3,30 @@ import java.util.Scanner;
 public class Gugudan {
 	public static void main(String[] args) {
 		
-		System.out.println("구구단 중 출력할 단은? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println(number * 1);
-		System.out.println(number * 2);
-		System.out.println(number * 3);
-		System.out.println(number * 4);
-		System.out.println(number * 5);
-		System.out.println(number * 6);
-		System.out.println(number * 7);
-		System.out.println(number * 8);
-		System.out.println(number * 9);
+		//7단
+		for(int j =  1; j < 10; j++) {
+			System.out.println(7 * j);
+		}
+		
+		//6단
+		int i = 1;
+		while(i < 10) {
+			System.out.println(6 * i);
+			i = i + 1;
+		}
+		
+//		System.out.println("구구단 중 출력할 단은? : ");
+//		Scanner scanner = new Scanner(System.in);
+//		int number = scanner.nextInt();
+//		System.out.println(number * 1);
+//		System.out.println(number * 2);
+//		System.out.println(number * 3);
+//		System.out.println(number * 4);
+//		System.out.println(number * 5);
+//		System.out.println(number * 6);
+//		System.out.println(number * 7);
+//		System.out.println(number * 8);
+//		System.out.println(number * 9);
 		
 		//4단
 //		System.out.println("4단");
