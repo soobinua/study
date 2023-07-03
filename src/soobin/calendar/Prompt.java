e1b412114570c8dbb249fdf02ddad4c1cf59d873
soobinua <dev7er@gmail.com>
2023-07-03 13:37:02 +0900

    일정 등록 / 검색 기능 구현하기 (2) - 구현 완료

--- src/soobin/calendar/Prompt.java
+++ src/soobin/calendar/Prompt.java
@@ -1,5 +1,6 @@
 package soobin.calendar;
 
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Prompt {
@@ -35,8 +36,8 @@ public class Prompt {
 //	| h. 도움말 q. 종료
 //	+----------------------+	
 //	명령 (1, 2, 3, h, q)
-	
-	public void runPrompt() {
+
+	public void runPrompt() throws ParseException {
 		printMenu();
 		
 		Scanner scanner = new Scanner(System.in);
@@ -45,11 +46,11 @@ public class Prompt {
 		while (true) {
 			System.out.println("명령을 입력하세요.(1, 2, 3, h, q)");
 			String cmd = scanner.next();
-			if(cmd.equals("1")) {
-				cmdRegister();
-			} else if(cmd.equals("2")) {
-				cmdSearch();
-			} else if(cmd.equals("3")) {
+			if (cmd.equals("1")) {
+				cmdRegister(scanner, cal);
+			} else if (cmd.equals("2")) {
+				cmdSearch(scanner, cal);
+			} else if (cmd.equals("3")) {
 				cmdCalendar(scanner, cal);
 			} else if(cmd.equals("h")) {
 				printMenu();
@@ -80,18 +81,37 @@ public class Prompt {
 		
 		c.printCalendar(year, month);
 	}
-	
-	private void cmdSearch() {
-		// TODO Auto-generated method stub
-		
+
+	private void cmdSearch(Scanner s, Calendar c) {
+		System.out.println("[일정 검색]");
+		System.out.println("날짜를 입력해 주세요.(yyyy-MM-dd)");
+		String date = s.next();
+
+		String plan = "";
+		try {
+			plan = c.searchPlan(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			System.err.print("일정 검색 중 오류가 발생했습니다.");
+		}
+
+		System.out.println(plan);
 	}
 
-	private void cmdRegister() {
-	// TODO Auto-generated method stub
-	
+	private void cmdRegister(Scanner s, Calendar c) throws ParseException {
+		System.out.println("[새 일정 등록]");
+		System.out.println("날짜를 입력해 주세요.(yyyy-MM-dd)");
+		String date = s.next();
+		String text = "";
+		s.nextLine(); // ignore one newline
+
+		System.out.println("일정을 입력해 주세요.");
+		text = s.nextLine();
+
+		c.registerPlan(date, text);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		// 셸 실행
 		Prompt p = new Prompt();
 		p.runPrompt();
