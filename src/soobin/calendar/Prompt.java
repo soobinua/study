c5818d1119e057795b75bec4f932af8dcc838c18
soobinua <dev7er@gmail.com>
2023-07-03 13:50:12 +0900

    코드 정리 - if else문 > switch case문으로 수정

--- src/soobin/calendar/Prompt.java
+++ src/soobin/calendar/Prompt.java
@@ -19,14 +19,24 @@ public class Prompt {
 	 * @return 0 ~ 6 (0 = Sunday, 6 = Saturday)
 	 */
 	public int parseDay(String week) {
-		if(week.equals("su")) return 0;
-		else if(week.equals("mo")) return 1;
-		else if(week.equals("tu")) return 2;
-		else if(week.equals("we")) return 3;
-		else if(week.equals("th")) return 4;
-		else if(week.equals("fr")) return 5;
-		else if(week.equals("sa")) return 6;
-		else return 0;
+		switch (week) {
+		case "su":
+			return 0;
+		case "mo":
+			return 1;
+		case "tu":
+			return 2;
+		case "we":
+			return 3;
+		case "th":
+			return 4;
+		case "fr":
+			return 5;
+		case "sa":
+			return 6;
+		default:
+			return 0;
+		}
 	}
 	
 //	+----------------------+
@@ -43,18 +53,25 @@ public class Prompt {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		while (true) {
+		boolean isLoop = true;
+		while (isLoop) {
 			System.out.println("명령을 입력하세요.(1, 2, 3, h, q)");
 			String cmd = scanner.next();
-			if (cmd.equals("1")) {
+
+			switch (cmd) {
+			case "1":
 				cmdRegister(scanner, cal);
-			} else if (cmd.equals("2")) {
+				break;
+			case "2":
 				cmdSearch(scanner, cal);
-			} else if (cmd.equals("3")) {
+				break;
+			case "3":
 				cmdCalendar(scanner, cal);
-			} else if(cmd.equals("h")) {
+				break;
+			case "h":
 				printMenu();
-			}  else if(cmd.equals("q")) {
+			case "q":
+				isLoop = false;
 				break;
 			}
 		}
