b5ea79550204dde53a4c6afd6ee2eaa1d79e9170
soobinua <dev7er@gmail.com>
2023-07-10 16:03:28 +0900

    비교연산자

---
+++ src/Operator11.java
@@ -0,0 +1,17 @@
+
+public class Operator11 {
+
+	public static void main(String[] args) {
+		// 비교연산자
+		System.out.println(2 < 3);
+		System.out.println(2 <= 3);
+		System.out.println(2 > 3);
+		System.out.println(3 >= 3);
+		System.out.println(5 == 3);
+		System.out.println(5 == 5);
+		System.out.println(5 != 5);
+		System.out.println(3 != 5);
+
+	}
+
+}
