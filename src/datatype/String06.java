4744be258c7480f9aebb32943cb6bf9b022d8262
soobinua <dev7er@gmail.com>
2023-07-10 17:02:16 +0900

    문자열 자르기 substring(시작위치, 끝위치(미포함))

---
+++ src/datatype/String06.java
@@ -0,0 +1,15 @@
+package datatype;
+
+public class String06 {
+
+	public static void main(String[] args) {
+		String birthday = "1970/01/01";
+		String year = birthday.substring(0, 4); 	// substring(시작위치, 끝위치(미포함)) 번호 0부터 시작
+		String monthAndDay = birthday.substring(5); // substring(시작위치)
+		String day = birthday.substring(8);
+		System.out.println("출생년도 : " + year);
+		System.out.println("출생월일 : " + monthAndDay);
+		System.out.println("출생일 : " + day);
+	}
+
+}
