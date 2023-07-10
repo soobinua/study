20dd41c3476bb83925c51793c8cab54ebb6cb3e8
soobinua <dev7er@gmail.com>
2023-07-10 17:05:57 +0900

    자료형 - 논리형

---
+++ src/datatype/Boolean01.java
@@ -0,0 +1,16 @@
+package datatype;
+
+public class Boolean01 {
+
+	public static void main(String[] args) {
+		// 자료형 - 논리형
+		boolean alwaysTrue = true; 		// 참
+		boolean alwaysFalse = false; 	// 거짓
+		boolean is5BiggerThan3 = 5 > 3;
+		System.out.println("alwaysTrue : " + alwaysTrue);
+		System.out.println("alwaysFalse : " + alwaysFalse);
+		System.out.println("5 is bigger than 3 : " + is5BiggerThan3);
+
+	}
+
+}
