3b3c6117ea1002d1c171127710d2b9f30a872b34
soobinua <dev7er@gmail.com>
2023-07-10 16:44:18 +0900

    자료형 - 문자

---
+++ src/datatype/Characters01.java
@@ -0,0 +1,12 @@
+package datatype;
+
+public class Characters01 {
+
+	public static void main(String[] args) {
+		// 자료형 - 문자
+		char character = 'o'; 	// 2 byte
+		System.out.println(character);
+
+	}
+
+}
