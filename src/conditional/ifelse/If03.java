bc16d6f47692d82531e1386f78770aec1f220f3b
soobinua <dev7er@gmail.com>
2023-07-11 12:59:54 +0900

    조건문 - if

---
+++ src/conditional/ifelse/If03.java
@@ -0,0 +1,13 @@
+package conditional.ifelse;
+
+public class If03 {
+
+	public static void main(String[] args) {
+		int money = 9000;
+		if (money >= 8000) {
+			System.out.println("순대국은 맛있다.");
+		}
+
+	}
+
+}
