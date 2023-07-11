cab7aa689064ce39b01da7e4ed2248ce70622593
soobinua <dev7er@gmail.com>
2023-07-11 13:01:42 +0900

    두 조건 중 일부만 만족해도 실행 - ||

---
+++ src/conditional/boolOperator/OrCases.java
@@ -0,0 +1,14 @@
+package conditional.boolOperator;
+
+public class OrCases {
+
+	public static void main(String[] args) {
+		// 논리합 연산자 - ||
+		System.out.println(false || false);
+		System.out.println(false || true);
+		System.out.println(true || false); // || 두 조건 중 하나만 true여도 true
+		System.out.println(true || true);
+
+	}
+
+}
