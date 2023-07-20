3855d8a3f7181a1ce86974ab1cc411b5301b757c
soobinua <dev7er@gmail.com>
2023-07-20 20:47:41 +0900

    컬렉션(Collection) - Set

---
+++ src/set/SetExample02.java
@@ -0,0 +1,18 @@
+package set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample02 {
+
+	public static void main(String[] args) {
+		// Set - 데이터 추가 add()
+		Set<String> myFavoriteFoods = new HashSet<>();
+
+		myFavoriteFoods.add("치킨");
+		myFavoriteFoods.add("피자");
+		myFavoriteFoods.add("치킨"); // 중복을 허용하지 않아 중복해서 데이터 추가하는 경우 무시
+
+		System.out.println(myFavoriteFoods);
+	}
+}
