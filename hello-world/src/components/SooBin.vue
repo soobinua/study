31608e2e331dd984d964dbe35f267c04cd198f97
soobinua <dev7er@gmail.com>
2023-08-14 14:55:37 +0900

    싱글 파일 컴포넌트 (Single File Component)

---
+++ hello-world/src/components/SooBin.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <button @click="updateName">Change Name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "SooBin",
+    }
+  },
+  methods: {
+    updateName() {
+      this.name = "SooBin Updated";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- hello-world/src/views/HomeView.vue
+++ hello-world/src/views/HomeView.vue
@@ -1,18 +1,29 @@
+<!--싱글 파일 컴포넌트 (Single File Component) 하나의 파일에 하나의 컴포넌트-->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>This is Home page</h1>
+    <SooBin></SooBin>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SooBin from "@/components/SooBin.vue";
 
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
-  }
-}
+    SooBin,
+  },
+  data() {
+    return {
+      name: "SooBin",
+    };
+  },
+};
 </script>
+
+<!--scoped 작성 시 이 템플릿에만 적용된다. 작성하지 않으면 전체에 적용된다. -->
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
